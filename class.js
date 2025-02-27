a233930b7cbe95c9f8eafc416684ea180151f13c
Niloykundu <164549379+Niloykundu@users.noreply.github.com>
2025-02-27 23:12:15 +0800

    submit

---
+++ class.js
@@ -0,0 +1,11 @@
+// product name,description,rating price ,discount (boolean)
+
+const product ={
+    Product : "Iphone",
+    Description : " Brand new smart phone with AI",
+    Rating: 4.5,
+    Price : 89999,
+    Discount : false
+}
+console.log(product);
+console.log(typeof(product));
