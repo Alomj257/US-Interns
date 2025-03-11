67765fff359bc7c6c9b1e80381721921abe7a8a1
Jahangir Alom <alomj257@gmail.com>
2025-03-11 16:01:23 +0530

    febo normal

--- Js/practice.js
+++ Js/practice.js
@@ -2,15 +2,28 @@
 
 // 0 1 1 2 3 5 8 13 21
 
-const febo = (n) => {
-    let feb = [0,1] // initilize first two number index = 0,1, 2,3,4,5,... 9 
-    // 0 - 0th index, 1- 1st index , 2- 0th index + first index = 1, 
+// const febo = (n) => {
+//     let feb = [0,1] // initilize first two number index = 0,1, 2,3,4,5,... 9 
+//     // 0 - 0th index, 1- 1st index , 2- 0th index + first index = 1, 
 
-    for(let i =2; i<n; i++){ //3
-        feb[i] = feb[i-1] + feb[i-2];
-    }
-    console.log(feb.join(","));
+//     for(let i =2; i<n; i++){ //3
+//         feb[i] = feb[i-1] + feb[i-2];
+//     }
+//     console.log(feb.join(","));
 
+// }
+
+// febo(10);
+
+
+let a =0;
+let b =1;
+
+console.log(a);
+console.log(b);
+for(let i=2; i<10; i++){
+    let temp = a+b;
+    console.log(temp);
+    a=b;
+    b=temp;
 }
-
-febo(10);
