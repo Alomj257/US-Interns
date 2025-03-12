60cfa9852c64e386de47fca544cfc99c0ed38ce1
.Ataguas <polnath471@gmail.com>
2025-03-12 13:05:56 +0530

    upto question no. 10 upadated

--- class.js
+++ class.js
@@ -100,7 +100,7 @@
 // console.log(a);
 // console.log(b);
 // for (let i=0;i<=10;i++){
-//     let temp = a + b;
+//     let temp = a + b;                                        
 //     console.log(temp);
 //     a = b;
 //     b = temp;
@@ -129,18 +129,122 @@
 
 // 3. Implement a program that finds the largest number in an array using a for loop.
 
-let num =prompt("Enter number of element you want to add in array");
-    let arr = [];
-    for(let i = 1 ; i<=num ; i++ ) {
-        arr.push(prompt("Enter the element"));
-    }
-    console.log(arr);
+// let num =prompt("Enter number of element you want to add in array");
+//     let arr = [];
+//     for(let i = 1 ; i<=num ; i++ ) {
+//         arr.push(prompt("Enter the element"));
+//     }
+//     console.log(arr);
 
-    maximum = arr[0];
+//     maximum = arr[0];
 
-    for(let i = 0 ; i<arr.length ; i++){
-        if(arr[i]>maximum){
-            maximum = arr[i];
-        }
-    }
-    console.log("The largest number in the array is",maximum);
+//     for(let i = 0 ; i<arr.length ; i++){
+//         if(arr[i]>maximum){
+//             maximum = arr[i];
+//         }
+//     }
+//     console.log("The largest number in the array is",maximum);
+
+
+
+// 4. Write a program to reverse a string using a for loop.
+
+// let str = prompt("Enter a string");
+// let rev = "";
+// for(let i = str.length -1 ; i>=0 ; i--){
+//     rev += str[i];
+// }
+// console.log(rev);
+
+// 5. Create a program that prints numbers from 1 to 100, but for multiples of 3, print 'Fizz' and for multiples of 5, print 'Buzz'. For numbers which are multiples of both 3 and 5, print 'FizzBuzz'.
+// for (i = 1 ; i<=100 ; i++){
+//     if (i % 3 == 0 && i % 5 ==0){
+//         console.log("FizzBuzz");
+//     }
+//     else if(i % 3 ==0){
+//         console.log("Fizz");
+//     }
+//     else if(i % 5 ==0){
+//         console.log("Buzz");
+//     }
+//     else{
+//         console.log(i);
+//     }   
+// }
+
+// 6. Write a program that calculates the factorial of a given number using a while loop.
+//  let num = prompt("Enter a number");
+// let fact = 1;
+//     while(num>1){
+//         fact *= num;
+//         num--;
+//     }
+//     console.log(fact);
+  
+// 7. Implement a program that checks if a number is prime using a for loop and conditional operators.
+
+// let num = prompt("Enter a number");
+// if(num < 2){
+//     console.log("Not a prime number");
+// }
+// for(let i = 2; i<num; i++){
+//     if(num % i == 0){
+//         console.log("Not a prime number");
+//     }else{
+//         console.log("Prime number");
+//     }
+// }
+
+// 8. Create a function that takes an array and a target number, and returns whether the target exists in the array using a for loop.
+
+// function findTarget(arr, target) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] === target) {
+//             return true; // Found the target
+//         }
+//     }
+//     return false; // Target not found
+// }
+// let noarray = prompt("enter the no of elements")
+// let array = []
+// for ( let i= 1; i<=noarray ; i++){
+//     array.push(prompt("enter the elements"));
+//     // return array;
+// }
+// let targetnum = prompt(parseInt("Enter the no u want to target"))
+// console.log(array);
+// findTarget(array,targetnum);
+
+
+// 9. Write a program to generate the multiplication table of a given number using a for loop
+
+
+// function multiplicationTable(num){
+//     for(let i= 1; i<=10 ; i++){
+//         console.log(`${num} * ${i} = ${num*i}`);
+//     }
+// }
+// let no = prompt("Enter the number");
+// multiplicationTable(no);
+
+// 10. Implement a function that removes duplicate elements from an array using a loop.
+
+
+// function duplicationofarray(arr){
+//     let unique =[];
+//     for(let i = 0 ; i<arr.length ; i++){
+//         if(!unique.includes(arr[i])){
+//             unique.push(arr[i]);
+//         }
+//     }
+//     return unique;
+// }
+// let num =prompt("Enter no. of element you want to add in array");
+// let array = [];
+//     for(let i = 1 ; i<=num ; i++ ) {
+//         array.push(prompt("Enter the element"));
+//     }
+// console.log(duplicationofarray(array));
+
+// 10.Write a program that counts the number of vowels in a given string using a for...of loop
+
